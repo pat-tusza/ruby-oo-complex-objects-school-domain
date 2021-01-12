3662c9184a1db5d398d439578e39714b5c9170d6
Patrick Tuszakowski <mrtall500@gmail.com>
2021-01-12 16:47:20 -0500

    commit tree 008ba0510d90fa72fa7100f01d16217e39521b04

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name 
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        new_hash = {}
+        roster.each do |grade, student|
+            new_hash[grade] = student.sort
+        end
+        new_hash
+    end
+end
